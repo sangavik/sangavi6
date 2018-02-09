812f9abd2db66653e18c07eb661c728894bb430c
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-09 15:45:35 +0530

    string.C

---
+++ string.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[i];
+int i,k;
+printf("\n enter the string:");
+scanf("%s",&a[i]);
+printf("\n enter the limit");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n ",a[i]);
+}
+}
