a0574ca432e76b650c487a572288f5b5efd14499
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-09 16:02:53 +0530

    binary.C

---
+++ binary.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count;
+char a[i];
+printf("\n enter the num");
+for(i=o;i<100;i++)
+{
+scanf("%s",a[i]);
+}
+for(i=0;i<100;i++)
+{
+if(a[i]%10==0/a[i]%10==1)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+}
+getch();
+}
