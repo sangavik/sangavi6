96e4bfb890c59aa3ecc61acc1b7ee6bf2e423149
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-09 16:21:04 +0530

    perfect.C

---
+++ perfect.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i;
+printf("\n enter a and b");
+scanf("%d %d"&a,&b);
+c=a*b;
+for(i=0;i<c;i++)
+{
+if(c==(i*i))
+{
+printf("yes\n);
+}
+else
+{
+printf("\n no):
+}
+}
+return 0;
+ }
